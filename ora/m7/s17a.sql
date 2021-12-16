219cb00657d5d7dfcceceef5d7611877916eb3b1
Emanuele <egalli64@gmail.com>
2021-12-16 12:22:22 +0100

    on delete cascade

--- ora/m7/s17a.sql
+++ ora/m7/s17a.sql
@@ -3,7 +3,7 @@ alter session set current_schema = hron;
 
 create table employee_salary (
     employee_id number(6, 0)
-    	references employee(employee_id),
+    	references employee(employee_id) on delete cascade,
     old_salary number(8, 2),
     new_salary number(8, 2)
 );
