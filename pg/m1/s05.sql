df04f8d3faefeb6f9637b588fdd7c0f695ef2fd5
Emanuele <egalli64@gmail.com>
2022-08-11 12:08:16 +0200

    more comments

--- pg/m1/s05.sql
+++ pg/m1/s05.sql
@@ -1,16 +1,21 @@
 -- Examples on logical operators
 
--- the other way round
+-- a plain select
 select *
 from employee
 where not department_id > 2;
 
--- both conditions should hold
+-- "not" is to get it the other way round
+select *
+from employee
+where not department_id > 2;
+
+-- "and" is when both conditions should hold
 select *
 from employee
 where salary < 3000 and employee_id > 160;
 
--- one or the other should hold
+-- "or" is when one or the other should hold
 select *
 from employee
 where salary < 2200 or last_name = 'King';
